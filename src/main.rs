14aca60a9c3ca21716e80c7e3154644ec1d2af19
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2017-12-01 22:47:57 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+use std::io::prelude::*;
+use std::path::*;
+use git2::
+
+extern crate git2;
+extern crate rocket;
+use rocket::response::NamedFile;
+
+const nin_project_repo = "https://github.com/ninjadev/zeven.git";
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/prods/<path..>")]
+fn get_prod(path: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("prods/").join(path)).ok()
+}
+
+fn main() {
+    println!("Hello, world!");
+    Repository::clone(nin_project_repo, )
+    rocket::ignite()
+        .mount("/", routes![index, get_prod])
+        .launch();
+}
